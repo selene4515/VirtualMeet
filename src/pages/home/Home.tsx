802399ddd62e5bec59a2fabccf33b9c562cec9e1
Marie / 정민지 <44185091+selene4515@users.noreply.github.com>
2023-02-22 16:46:06 +0900

    Fix : Home 내부 컴포넌트 export로 변경

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -24,7 +24,7 @@ const Home = () => {
 
 const mainBgColor: string =
   "linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 100%),linear-gradient(90deg, #4ac8ff 0%, #7fff6a 100%)";
-const Main = styled.div`
+export const Main = styled.div`
   background: ${(props) => props.color || "#000000"};
   width: 100%;
   display: flex;
@@ -32,13 +32,13 @@ const Main = styled.div`
   align-items: center;
 `;
 
-const Container = styled.div`
-  border: 1px solid red;
+export const Container = styled.div`
   width: 1024px;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 export default Home;
