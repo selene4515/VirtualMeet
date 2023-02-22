6b5cbc0900b6cbd360d0be1c5a8638708499f3a4
Marie / 정민지 <44185091+selene4515@users.noreply.github.com>
2023-02-22 16:41:17 +0900

    Fix : HomeTogBg Component 플랜이동버튼 수정

--- src/pages/home/HomeTopBg.tsx
+++ src/pages/home/HomeTopBg.tsx
@@ -6,8 +6,8 @@ const HomeTopBg = () => {
     <TopBg>
       <SubTitleTxt>Next Level Communication</SubTitleTxt>
       <TitleTxt>VIRTUAL MEET</TitleTxt>
-      <Link to="/">
-        <DownloadBtn>Download</DownloadBtn>
+      <Link to="/plan">
+        <StartedBtn>Get Started!</StartedBtn>
       </Link>
       <MainImg
         src={require("../../assets/home/T_mainImgxl.svg").default}
@@ -38,19 +38,19 @@ const TitleTxt = styled.div`
   margin-bottom: 2.5rem;
 `;
 
-const DownloadBtn = styled.button`
+const StartedBtn = styled.button`
   padding: 1rem 2.25rem;
   gap: 0.75rem;
 
-  width: 17.625rem;
-  height: 3.5rem;
+  width: 12.8125rem;
+  height: 3.5625rem;
 
   background: rgba(255, 255, 255, 0.13);
   border-radius: 0.375rem;
 
   font-weight: 600;
-  font-size: 1.125rem;
-  line-height: 1.4375rem;
+  font-size: 1.375rem;
+  line-height: 1.75rem;
 
   border: 1px solid;
 `;
