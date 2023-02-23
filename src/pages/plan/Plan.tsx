299e28056291b28b58cb6f96ce4e4b8b3b8c7d78
Marie / 정민지 <44185091+selene4515@users.noreply.github.com>
2023-02-23 12:42:08 +0900

    Fix : Plan Component 타이틀 수정

--- src/pages/plan/Plan.tsx
+++ src/pages/plan/Plan.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Container, Main } from "../home/Home";
 import PlanCard from "./PlanCard";
@@ -6,7 +7,12 @@ const Plan = () => {
   return (
     <Main>
       <Container>
-        <TitleTxt>VIRTUAL MEET Plan</TitleTxt>
+        <TitleTxt>VIRTUAL MEET Pricing</TitleTxt>
+        <SubTitleTxt>
+          For more information regarding Zoom’s cancellation policies,
+          <br /> see Virtual Meet’s
+          <Link to="https://explore.zoom.us/en/terms/"> terms of service</Link>.
+        </SubTitleTxt>
         <div>
           <PlanCard
             color="#7FFF6A"
@@ -29,7 +35,22 @@ const TitleTxt = styled.div`
   font-size: 40px;
   line-height: 50px;
   margin-top: 2.75rem;
-  margin-bottom: 3.25rem;
+`;
+const SubTitleTxt = styled.div`
+  font-weight: 600;
+  font-size: 0.5625rem;
+  line-height: 140%;
+
+  margin-top: 1rem;
+  margin-bottom: 3.375rem;
+  text-align: center;
+
+  color: #a4b4be;
+
+  a {
+    text-decoration: underline;
+    color: #a4b4be;
+  }
 `;
 
 export default Plan;
